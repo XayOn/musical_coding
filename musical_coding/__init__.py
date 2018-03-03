edf28415d7710ccdd47c0fc0b5244306718c1866
David Francos <davidfrancos@sipay.es>
2018-03-03 17:40:07 +0100

    Reimplementation, again.

--- musical_coding/__init__.py
+++ musical_coding/__init__.py
@@ -1,93 +1,141 @@
-"""MusicalCoding.
+""" Constants.
 
-Convert any piece of code to its musical representation.
+Basic definition, syntetizing piano.
+
+TODO: Decide how to implement accords.
+TODO: Add more instruments.
+TODO: I have the score, now find a better way to represent it...
 """
 
+from collections import namedtuple
 from functools import lru_cache
-import logging
-
 from docopt import docopt
 import fluidsynth
 import numpy
 import pyaudio
-import tokenize
 import soundfile
+import tokenize
 
-# SFILE = '/usr/share/sounds/sf2/FluidR3_GM.sf2'
 SFILE = "/usr/share/sounds/sf2/TimGM6mb.sf2"
 
-logging.basicConfig(level=logging.DEBUG)
+NOTES = {
+    0: 60,  # A
+    1: 68,  # B
+    2: 76,  # C
+    3: 82,  # D
+    4: 90,  # E
+    5: 98,  # F
+    6: 116,  # G
+}
+
+NOTE_NAMES = {
+    NOTES[0]: 'A',
+    NOTES[1]: 'B',
+    NOTES[2]: 'C',
+    NOTES[3]: 'D',
+    NOTES[4]: 'E',
+    NOTES[5]: 'F',
+    NOTES[6]: 'G',
+}
+
+SCALES = (1, 2, 3)
+
+DURATIONS = (
+    16 / 8,  # breve
+    8 / 8,  # semibreve
+    4 / 8,  # minim
+    2 / 8,  # crotchet
+    1 / 8,  # quaver
+    1 / 16,  # semiquaver
+    1 / 32,  # demisemiquaver
+)
+
+DURATION_NAMES = {
+    DURATIONS[0]: 'breve',
+    DURATIONS[1]: 'semibreve',
+    DURATIONS[2]: 'minim',
+    DURATIONS[3]: 'crotchet',
+    DURATIONS[4]: 'quaver',
+    DURATIONS[5]: 'semiquaver',
+    DURATIONS[6]: 'demisemiquaver',
+}
+
+
+class Note(namedtuple('Note', 'value, scale, duration, line')):
+    def __repr__(self):
+        return "Note({}, scale={}, duration={})".format(
+            NOTE_NAMES[self.value], self.scale, DURATION_NAMES[self.duration])
+
+
+def get_normalized_note(max_note: Note, note: Note) -> Note:
+    """Normalize a note."""
+
+    def scale(old_value, old_max, new_max):
+        """Scale."""
+        return (old_value / old_max) * new_max
+
+    value = NOTES[int(scale(note.value, max_note.value, len(NOTES) - 1))]
+    scale_ = SCALES[int(scale(note.scale, max_note.scale, len(SCALES) - 1))]
+    duration = DURATIONS[int(
+        scale(note.duration, max_note.duration,
+              len(DURATIONS) - 1))]
+
+    return Note(value, scale_, duration, note.line)
+
+
+def get_tokenization(filename):
+    note, value = 0, 0
+    enum = enumerate(tokenize.generate_tokens(open(filename).readline))
+    for num, token in enum:
+        # TODO: A "strict" mode should be implemented that does not ignore
+        # anything
+        if token.line == '\n':
+            # Avoid empty lines.
+            continue
+        if 'import' in token.line and num < 400:
+            # ignore imports... they all sound equal and they're boring
+            # at the song start
+            continue
+        if token.type == 3:
+            # ingore docstrings, they're too large
+            continue
+        if token.string == '\n':
+            yield note, 1, value, token.line
+            note, value = 0, 0
+        else:
+            note += token.type
+            value += len(token.string)
 
 
 class MusicalCodeFile:
-    """MUsical Code File."""
+    """Musical Code File."""
 
-    max_num = 7
-
-    def __init__(self, filename, sfile=SFILE, freq=44100, max_note_speed=5,
-                 start_note=10, start_duration=0):
-        self.start_note = start_note
-        self.start_duration = start_duration
-        self.file = open(filename).readlines()
+    def __init__(self,
+                 filename,
+                 sfile=SFILE,
+                 note_speed=20,
+                 freq=44100,
+                 speed_multiplier=0.8):
         self.freq = freq
         self.fluid = fluidsynth.Synth()
         sfid = self.fluid.sfload(sfile)
-        self.tokens = list(tokenize.generate_tokens(open(filename).readline))
-        self.maxsize = max([len(a.string) for a in self.tokens])
-        self.midsize = int(self.maxsize * 0.55)
-        if self.midsize > max_note_speed:
-            self.midsize = max_note_speed
-        logging.debug("Max note duration: %s", self.midsize)
         self.fluid.program_select(0, sfid, 0, 0)
-        self.note_speed = 20
-
-    def adjust_note(cls, value):
-        if value > 100:
-            value -= 100
-        elif value < 40:
-            value += 20
-        return int(value)
-
-    def adjust_duration(self, value):
-        result = value / 2
-
-        if result > self.midsize:
-            while result > self.midsize:
-                result -= self.midsize
-
-        if result == 0:
-            result = 0.3
-
-        return result
-
-    @property
-    @lru_cache()
-    def notes(self):
-        """Notes."""
-        note = self.start_note
-        value = self.start_duration
-        line = ''
-        for token in self.tokens:
-            if token.string == '\n':
-                yield (self.adjust_note(note),
-                       self.adjust_duration(value),
-                       line)
-                note = 0
-                value = 0
-                line = ''
-            else:
-                note += token.type
-                value += len(token.string)
-                line += ' ' + token.string
+        self.note_speed = note_speed  # Note speed, as fluidsynth specifies
+        self.speed_multiplier = speed_multiplier
+        notes = list(Note(*args) for args in get_tokenization(filename))
+        max_note = Note(
+            max(note.value for note in notes),
+            max(note.scale for note in notes),
+            max(note.duration for note in notes), '')
+        self.notes = list(
+            get_normalized_note(max_note, note) for note in notes)
+        print(self.notes)
 
     def audio_stream(self):
         """Get audio stream."""
         pa = pyaudio.PyAudio()
         return pa.open(
-            format=pyaudio.paInt16,
-            channels=2,
-            rate=self.freq,
-            output=True)
+            format=pyaudio.paInt16, channels=2, rate=self.freq, output=True)
 
     def play(self):
         self.audio_stream().write(self.audio)
@@ -97,40 +145,16 @@ class MusicalCodeFile:
     def numpy_array(self):
         """To numpy arrays."""
         curr = []
-        curr_notes = []
-        pending_notes = []
-        for note, duration, line in self.notes:
-            logging.debug("line <<<%s>>> produced note %s duration %s",
-                          line, note, duration)
-
-            # If duration is less than three, we assume it's a background one.
-            if duration <= 0.3:
-                pending_notes.append((0, note, int(self.note_speed / 2)))
-            else:
-                curr_notes.append((0, note, self.note_speed))
-                # May add some curr_notes logic here too
-                for note_a in curr_notes:
-                    self.fluid.noteon(*note_a)
-                    for note in pending_notes:
-                        # Maybe remove too-close octaves?
-                        self.fluid.noteon(*note)
-
-                    pending_notes = []
-
-                    curr = numpy.append(
-                        curr, self.fluid.get_samples(
-                            int((self.freq * duration) / 2)))
-
-                    self.fluid.noteoff(*note_a[:-1])
-
-                    curr = numpy.append(
-                        curr, self.fluid.get_samples(
-                            int(self.freq * (duration / 2))))
-
-                    for note in pending_notes:
-                        self.fluid.noteoff(*note)
-
-                curr_notes = []
+        length = len(self.notes)
+        for p, note in enumerate(self.notes):
+            percent = "{:0.0f}%".format(numpy.ceil((p / length) * 100))
+            print(percent, end='\033[20D', flush=True)
+            self.fluid.noteon(0, note.value, self.note_speed)
+            curr = numpy.append(
+                curr,
+                self.fluid.get_samples(
+                    int((self.freq * note.duration * self.speed_multiplier))))
+            self.fluid.noteoff(0, note.value)
         return curr
 
     @property
